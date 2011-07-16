f2f1ffc02a51ff74b8d5e62053dfa79d2ba3f645
David Sosby <dsosby@gmail.com>
2011-07-16 14:18:50 -0500

    Adding solution to problem 2

---
+++ prob2.clj
@@ -0,0 +1,17 @@
+;Project Euler - Problem 2
+;Sum all even Fibonacci numbers < 4000000
+
+(defn even-fibonnaci
+  {:doc "Generate a lazy sequence of even Fibonacci numbers less than [max]"}
+  ([max] (even-fibonnaci 1 0 max))
+  ([cur prev max]
+    (let [fib (+ cur prev)]
+      (if (< fib max)
+        (do
+          (if (even? fib)
+            (lazy-seq (cons fib (even-fibonnaci fib cur max)))
+            (recur fib cur max)))
+        nil))))
+
+(time (println "Result=" (reduce + (even-fibonnaci 4000000))))
+
