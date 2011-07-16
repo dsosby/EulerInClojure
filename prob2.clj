8e672dcfa6442cfef708580f20f881cddaaf37b6
David Sosby <dsosby@gmail.com>
2011-07-16 14:22:49 -0500

    After re-reading the code, realized the (do) was superfluous

--- prob2.clj
+++ prob2.clj
@@ -7,10 +7,9 @@
   ([cur prev max]
     (let [fib (+ cur prev)]
       (if (< fib max)
-        (do
-          (if (even? fib)
-            (lazy-seq (cons fib (even-fibonnaci fib cur max)))
-            (recur fib cur max)))
+        (if (even? fib)
+          (lazy-seq (cons fib (even-fibonnaci fib cur max)))
+          (recur fib cur max))
         nil))))
 
 (time (println "Result=" (reduce + (even-fibonnaci 4000000))))
