9c3f2c7330f42da98e1b03de04aba3303b607970
David Sosby <dsosby@gmail.com>
2011-07-17 11:14:22 -0500

    Adding in a program entry point

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject euler "1.0.0"
   :description "Implementations of Project Euler problems in Clojure."
   :url "http://github.com/dsosby/EulerInClojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :main euler)
---
+++ src/euler.clj
@@ -0,0 +1,7 @@
+(ns euler)
+
+(defn -main [& args]
+  (do
+    ;(println "Problem 1" 1234)
+    (println "This will print out the results of all solved problems")
+  ))
